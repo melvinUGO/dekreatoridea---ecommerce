206db3760da6dd2d105fb9fa50f36997c3054963
melvin <ugochukwumelvin@email.com>
2023-06-02 10:48:55 +0100

    fix

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -2,8 +2,8 @@ import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
 import { NextResponse } from "next/server";
 
-export async function GET(request) {
-  await mongooseConnect();
+// export async function GET(request) {
+//   await mongooseConnect();
 
-  return NextResponse("ok");
-}
+//   return NextResponse("ok");
+// }
